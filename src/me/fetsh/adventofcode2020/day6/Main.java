4a7229cf2dc1e539032e0a18ab17debee35216bc
fetsh-edu <fetsh.me@gmail.com>
2020-12-06 12:47:45 +0300

    Day 6: Add comments :)

--- src/me/fetsh/adventofcode2020/day6/Main.java
+++ src/me/fetsh/adventofcode2020/day6/Main.java
@@ -10,17 +10,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         var groupAnswers = File.readAllBytes(Main.class.getResource("input.txt").getPath()).split("\n\n");
 
+        // Part 1 Version 1
         var sum1_0 = Arrays.stream(groupAnswers)
                 .mapToInt(ga -> (int) Arrays.stream(ga.split("")).filter(s -> !s.isBlank()).distinct().count())
                 .sum();
         System.out.println(sum1_0);
 
+        // Part 1 Version 2
         var sum1_1 = Arrays.stream(groupAnswers)
                 .mapToInt(ga -> Arrays.stream(ga.split("")).filter(s -> !s.isBlank()).collect(Collectors.toSet()).size() )
                 .sum();
         System.out.println(sum1_1);
 
-
+        // Part 2
         var sum2 = Arrays.stream(groupAnswers)
                 .mapToInt(ga -> {
                     var personAnswers = ga.split("\n");
